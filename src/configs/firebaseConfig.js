d090a9463f431800a18db413952550bb4d6b43af
Davor Pernjek <pernjek.davor12@gmail.com>
2021-10-21 18:37:08 +0200

    remove firebase config

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAKIYF2qAgj2Jr4VHqkzDpEYN4SCdton2o",
-  authDomain: "test-773e2.firebaseapp.com",
-  projectId: "test-773e2",
-  storageBucket: "test-773e2.appspot.com",
-  messagingSenderId: "39392614563",
-  appId: "1:39392614563:web:bfe787922950427dd2c716",
-  measurementId: "G-HJEPV28WYB",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
